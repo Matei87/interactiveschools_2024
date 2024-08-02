eadffde5f62c0d085372ed12df7e285536a5c43b
Matei87 <mrc_matei@yahoo.com>
2024-08-02 19:06:49 +0300

    added styling part 2

--- app/rotatorCollection.js
+++ app/rotatorCollection.js
@@ -2,25 +2,25 @@ export const rotatorCollection = [
   {
     link: 'https://myanimelist.net/anime/1/Cowboy_Bebop',
     image: 'https://images.dog.ceo/breeds/segugio-italian/n02090722_002.jpg',
-    description: 'Product 1 description',
+    description: 'Product description',
     title: 'Cowboy',
   },
   {
     link: 'https://myanimelist.net/anime/5/Cowboy_Bebop__Tengoku_no_Tobira',
     image: 'https://images.dog.ceo/breeds/labradoodle/Cali.jpg',
-    description: 'Product 2 description',
+    description: 'Product description',
     title: 'Bebop',
   },
   {
     link: 'https://myanimelist.net/anime/6/Trigun',
     image: 'https://images.dog.ceo/breeds/spaniel-irish/n02102973_2870.jpg',
-    description: 'Product 3 description',
+    description: 'Product description',
     title: 'Trigun',
   },
   {
     link: 'https://myanimelist.net/anime/7/Witch_Hunter_Robin',
     image: 'https://images.dog.ceo/breeds/labrador/IMG_2397.jpg',
-    description: 'Product 4 description',
+    description: 'Product description',
     title: 'Witch Hunter',
   },
 ];
